907949138447fc676129628f23e8cb776ce5e3c7
anatsrii <anat.allsolution@gmail.com>
2025-02-26 22:20:24 +0700

    Add BlogsContent.jsx

---
+++ src/components/BlogsContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function BlogsContent() {
+  return (
+    <div className='blogs-content-container'>
+      <div className='blogs-content-header'>
+        <button className='toggle-btn'>X</button>
+      </div>
+      BlogsContent
+    </div>
+      
+  )
+}
+
+export default BlogsContent
