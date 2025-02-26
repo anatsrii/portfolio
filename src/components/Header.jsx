f3766e97eeae2a5e480073904248a7d44500049d
anatsrii <anat.allsolution@gmail.com>
2025-02-26 22:17:14 +0700

    Add function handlerBlogs.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,49 @@
-import React from 'react';
 import computerIcon from '../assets/computer.png';
 import profileIcon from '../assets/profile.png';
 import robotIcon from '../assets/robot.png';
-import mailIcon from '../assets/mail.png';
-import '../styles/hearder.css';
+import mailIcon from '../../email5rm.png';
+import '../styles/header.css';
 import blog from '../assets/blog.png';
+import { useDispatch } from 'react-redux';
+import { toggleContact, toggleHome, toggleAbout, toggleProjects, toggleBlogs} from '../stores/pageReducer';
+import { useSelector } from 'react-redux';
+
 
 function Header() {
+  const { home,contact, about, projects, blogs } = useSelector((state) => state.pages);
+  const dispatch = useDispatch();
+
+  const handlerBlogs = () => {
+    dispatch(toggleContact(true));
+    dispatch(toggleHome(true));
+    dispatch(toggleAbout(true));
+    dispatch(toggleProjects(true));
+    dispatch(toggleBlogs(blogs));
+  }
+
   return (
     <nav>
       <ul>
-        <li>
+        <li onClick={() => dispatch(toggleHome(home))}>
           <img src={computerIcon} alt="Home" />
           <span className='nav-text'>Home</span>
         </li>
-        <li>
+        <li onClick={() => dispatch(toggleAbout(about))}>
           <img src={profileIcon} alt="About" />
           <span className='nav-text'>About</span>
         </li>
-        <li>
+        <li onClick={() => dispatch(toggleContact(contact))}>
+  
           <img src={mailIcon} alt="Contact" />
           <span className='nav-text'>Contact</span>
+     
         </li>
-        <li>
+
+        <li onClick={() => dispatch(toggleProjects(projects))}>
           <img src={robotIcon} alt="Projects" />
           <span className='nav-text'>Projects</span>
         </li>
-        <li>
+        <li onClick={() => handlerBlogs()}>
           <img src={blog} alt="Blog" />
           <span className='nav-text'>Blog</span>
         </li>
