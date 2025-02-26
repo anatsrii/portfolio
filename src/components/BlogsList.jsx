2053b4a83d96781ad1bbf21540e3ed5d4319a1ca
anatsrii <anat.allsolution@gmail.com>
2025-02-26 22:18:42 +0700

    Add BlogsList.jsx component.

---
+++ src/components/BlogsList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function BlogsList () {
+  return (
+    <div className='blogs-list-container'>
+      <div className='blogs-list-header'>
+        <button className='toggle-btn'>X</button>
+      </div>
+        Blogs List
+
+    </div>
+  )
+}
+
+export default BlogsList
